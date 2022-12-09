053c74f5395871886f8638363b789267f1a20d49
Anna <anna49.fog@yandex.ru>
2022-12-09 13:18:35 +0100

    feat: add dynamic year to copyright

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,91 +1,94 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	svg4everybody();
+  svg4everybody();
 
 
 // Slider
 
-	$('.slider').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		speed: 1100,
-		dots: false,
-		prevArrow: false,
-		nextArrow: '<button type="button" class="slick-next"><img src="img/svg/arrow-right.svg"></button>',
-		swipeToSlide: true,
-		responsive: [{
-			breakpoint: 1181,
-			settings: {
-				slidesToShow: 2
-			}
-		}, {
-			breakpoint: 991,
-			settings: {
-				slidesToShow: 1
-			}
-		}]
-	});
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 1100,
+    dots: false,
+    prevArrow: false,
+    nextArrow: '<button type="button" class="slick-next"><img src="img/svg/arrow-right.svg"></button>',
+    swipeToSlide: true,
+    responsive: [{
+      breakpoint: 1181,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
 
 
+  // Burger-menu
 
-	// Burger-menu
-
-	const burger = document.querySelector('.burger');
-	const span = burger.querySelectorAll('span');
-	const overlay = document.querySelector('.overlay');
-	const mobileMenu = document.querySelector('.mobile-menu');
-
-
-	const toggleMobileMenu = function() {
-		overlay.classList.toggle('active-overlay');
-		burger.classList.toggle('active-burger');
-		span.forEach(item => item.classList.toggle('active-span'));
-	};
-
-		burger.addEventListener('click', function () {
-		toggleMobileMenu("add");
-	});
-
-	const menuItems = mobileMenu.querySelectorAll('li a');
-
-	menuItems.forEach(item => {
-		item.addEventListener('click', toggleMobileMenu);
-	});
-
-	document.addEventListener('keyup', function (e) {
-		if(overlay.classList.contains('active-overlay') && e.code === 'Escape') {
-			toggleMobileMenu();
-		}
-	});
+  const burger = document.querySelector('.burger');
+  const span = burger.querySelectorAll('span');
+  const overlay = document.querySelector('.overlay');
+  const mobileMenu = document.querySelector('.mobile-menu');
 
 
+  const toggleMobileMenu = function () {
+    overlay.classList.toggle('active-overlay');
+    burger.classList.toggle('active-burger');
+    span.forEach(item => item.classList.toggle('active-span'));
+  };
 
-	// Pageup
+  burger.addEventListener('click', function () {
+    toggleMobileMenu("add");
+  });
 
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 1200) {
-			$('.pageup').fadeIn("slow");
-		} else {
-			$('.pageup').fadeOut("slow");
-		}
-	});
+  const menuItems = mobileMenu.querySelectorAll('li a');
 
+  menuItems.forEach(item => {
+    item.addEventListener('click', toggleMobileMenu);
+  });
 
-	// Smooth scroll
-
-	$('a[href^="#"]').on('click', function() {
-		let href = $(this).attr('href');
-		$('html, body').animate({
-			scrollTop: $(href).offset().top
-		}, 400);
-		return false;
-	});
+  document.addEventListener('keyup', function (e) {
+    if (overlay.classList.contains('active-overlay') && e.code === 'Escape') {
+      toggleMobileMenu();
+    }
+  });
 
 
-	// Animation
+  // Pageup
 
-	AOS.init();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1200) {
+      $('.pageup').fadeIn("slow");
+    } else {
+      $('.pageup').fadeOut("slow");
+    }
+  });
 
+
+  // Smooth scroll
+
+  $('a[href^="#"]').on('click', function () {
+    let href = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(href).offset().top
+    }, 400);
+    return false;
+  });
+
+
+  // Animation
+
+  AOS.init();
+
+
+  // set current year in copyright
+  const today = new Date();
+  const copyright = document.querySelector('.message__copyright span')
+  copyright.innerHTML = today.getFullYear().toString()
 });
 
 
